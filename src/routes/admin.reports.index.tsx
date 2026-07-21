9d4d92b8991c1078d1d0f127dc5cd1465c428145
steveasterafovo <steveasterafovo@gmail.com>
2026-07-21 19:39:19 +0100

    d

--- src/routes/admin.reports.index.tsx
+++ src/routes/admin.reports.index.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { toast } from "sonner";
 import { AdminShell } from "@/layouts/AdminShell";
 import { PageHeader, ConfirmDelete } from "@/components/site/AdminBits";
-import { DataTable } from "@/components/admin/DataTable";
+import { DataTable } from "@/components/site/DataTable";
 import { reportsApi } from "@/api/extra.api";
 import type { Report, ReportStatus } from "@/types";
 

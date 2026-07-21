8cf94d9819813112843fa6fb619b2592202d9b63
steveasterafovo <steveasterafovo@gmail.com>
2026-07-21 19:16:32 +0100

    s

---
+++ src/stores/useCategoriesStore.ts
@@ -0,0 +1,19 @@
+import { createResourceStore } from "./createResourceStore";
+import type { APIAdminCategory, AdminCategoryPayload } from "@/data/admin-categories";
+
+const store = createResourceStore<APIAdminCategory, AdminCategoryPayload>({
+  resourceKey: "admin-categories",
+  basePath: "categories",
+});
+
+export const fetchAdminCategories = store.fetchList;
+export const fetchAdminCategoryById = store.fetchById;
+export const createAdminCategory = store.createItem;
+export const updateAdminCategory = store.updateItem;
+export const deleteAdminCategory = store.removeItem;
+
+export const useAdminCategoriesList = store.useList;
+export const useAdminCategoryDetail = store.useDetail;
+export const useCreateAdminCategory = store.useCreate;
+export const useUpdateAdminCategory = store.useUpdate;
+export const useDeleteAdminCategory = store.useRemove;

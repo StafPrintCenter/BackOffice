9b488b8d855f32fa83fcffa9b4e2b0f57b730d2a
steveasterafovo <steveasterafovo@gmail.com>
2026-07-21 19:17:50 +0100

    d

--- src/routes/admin/articles/index.tsx
+++ src/routes/admin/articles/index.tsx
@@ -10,7 +10,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { useAdminArticlesList, useCreateAdminArticle, useDeleteAdminArticle } from "@/stores/useArticlesStore";
-import { useAdminCategoriesList } from "@/stores/useAdminCategoriesStore";
+import { useAdminCategoriesList } from "@/stores/useCategoriesStore";
 import type { APIAdminArticleListItem, AdminArticlePayload } from "@/data/articles";
 import { RichTextEditor } from "@/components/admin/RichTextEditor";
 

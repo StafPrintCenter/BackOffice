52dbeeb8d22ab51f7bbf039f25dfc3028b494b36
steveasterafovo <steveasterafovo@gmail.com>
2026-07-22 03:20:34 +0100

    s

---
+++ src/stores/useTrainingsStore.ts
@@ -0,0 +1,19 @@
+import { createResourceStore } from "./createResourceStore";
+import type { APIAdminTrainingDetail, AdminTrainingPayload } from "@/data/trainings";
+
+const store = createResourceStore<APIAdminTrainingDetail, AdminTrainingPayload>({
+  resourceKey: "trainings",
+  basePath: "trainings",
+});
+
+export const fetchAdminTrainings = store.fetchList;
+export const fetchAdminTrainingById = store.fetchById;
+export const createAdminTraining = store.createItem;
+export const updateAdminTraining = store.updateItem;
+export const deleteAdminTraining = store.removeItem;
+
+export const useAdminTrainingsList = store.useList;
+export const useAdminTrainingDetail = store.useDetail;
+export const useCreateAdminTraining = store.useCreate;
+export const useUpdateAdminTraining = store.useUpdate;
+export const useDeleteAdminTraining = store.useRemove;

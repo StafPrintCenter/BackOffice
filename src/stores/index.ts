548123ee63b69778c0c635c262b5350770b58965
steveasterafovo <steveasterafovo@gmail.com>
2026-07-22 13:43:54 +0100

    s

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,5 +2,6 @@ export { createResourceStore } from "./createResourceStore";
 export * from "./useAuthStore";
 export * from "./useArticlesStore";
 export * from "./useCategoriesStore";
+export * from "./useProjectsStore";
 export * from "./useShortLinksStore";
 export * from "./useTrainingsStore";

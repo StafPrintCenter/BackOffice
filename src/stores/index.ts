bceb2e009351d79d1f85bf9f1016344e50b0f4aa
steveasterafovo <steveasterafovo@gmail.com>
2026-07-23 01:19:24 +0100

    s

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -5,7 +5,7 @@ export * from "./useArticlesStore";
 export * from "./useCategoriesStore";
 export * from "./useContactsStore";
 export * from "./useFaqsStore";
-export * from "./useNewsletterSubscribersStore";
+export * from "./useNewsletterCampaignsStore.ts";
 export * from "./useNewsletterSubscribersStore";
 export * from "./useProjectsStore";
 export * from "./useReportsStore";

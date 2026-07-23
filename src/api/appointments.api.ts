5326cf757fa37e1bcfccf32b31616a7a02f2ad1c
steveasterafovo <steveasterafovo@gmail.com>
2026-07-23 22:06:28 +0100

    ff

--- src/api/appointments.api.ts
+++
@@ -1,5 +0,0 @@
-import type { Appointment } from "@/types";
-import { seedAppointments } from "@/data/seedAppointments";
-import { makeApi } from "./_makeApi";
-
-export const appointmentsApi = makeApi<Appointment>(seedAppointments);

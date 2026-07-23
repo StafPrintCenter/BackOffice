e7edea02e8264030280d31a9d25c48f1eef7381c
steveasterafovo <steveasterafovo@gmail.com>
2026-07-23 22:51:09 +0100

    d

--- src/api/extra.api.ts
+++ src/api/extra.api.ts
@@ -1,7 +1,6 @@
-import type { Stat, ShortLink, Report, } from "@/types";
-import { seedStats, seedShortLinks, seedReports } from "@/data/seedExtra";
+import type { Stat, Report, } from "@/types";
+import { seedStats, seedReports } from "@/data/seedExtra";
 import { makeApi } from "./_makeApi";
 
 export const statsApi = makeApi<Stat>(seedStats);
-export const shortLinksApi = makeApi<ShortLink>(seedShortLinks);
 export const reportsApi = makeApi<Report>(seedReports);

db8e42391fa9e36e5e589e03bb1597bf7224b2f7
steveasterafovo <steveasterafovo@gmail.com>
2026-07-23 23:52:49 +0100

    n

--- src/api/extra.api.ts
+++ src/api/extra.api.ts
@@ -2,4 +2,3 @@ import type { Report, } from "@/types";
 import { seedReports } from "@/data/seedExtra";
 import { makeApi } from "./_makeApi";
 
-export const reportsApi = makeApi<Report>(seedReports);

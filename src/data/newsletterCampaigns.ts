2202adac99de6babb4782890346ca091a4b68ffd
steveasterafovo <steveasterafovo@gmail.com>
2026-07-23 01:52:45 +0100

    s

--- src/data/newsletterCampaigns.ts
+++ src/data/newsletterCampaigns.ts
@@ -1,4 +1,4 @@
-export type NewsletterCampaignStatus = "draft" | "scheduled" | "sent" | string;
+export type NewsletterCampaignStatus = "draft" | "scheduled" | "sent";
 
 export type APIAdminNewsletterCampaignListItem = {
   id: string;

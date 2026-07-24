40b8491eae294f6cb87f25bd51f80e2149bcf972
steveasterafovo <steveasterafovo@gmail.com>
2026-07-24 16:16:34 +0100

    s

--- src/routes/admin/trainings/registrations/$id.tsx
+++ src/routes/admin/trainings/registrations/$id.tsx
@@ -7,17 +7,8 @@ import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import {
-  useAdminTrainingRegistrationDetail,
-  useUpdateAdminTrainingRegistrationStatus,
-} from "@/stores/useTrainingRegistrationsStore";
-import {
-  type TrainingRegistrationStatus,
-  type AdminTrainingRegistrationStatusPayload,
-  getStatusBadge,
-  getStatusLabel,
-  STATUS_LABELS,
-} from "@/data/trainingRegistrations";
+import { useAdminTrainingRegistrationDetail, useUpdateAdminTrainingRegistrationStatus, } from "@/stores/useTrainingRegistrationsStore";
+import { type TrainingRegistrationStatus, type AdminTrainingRegistrationStatusPayload, getStatusBadge, getStatusLabel, STATUS_LABELS, } from "@/data/trainingRegistrations";
 import { SITE } from "@/data/site";
 
 export const Route = createFileRoute("/admin/trainings/registrations/$id")({

5266a466428773d0407dc8b98de577d5d6b4c973
steveasterafovo <steveasterafovo@gmail.com>
2026-07-27 14:42:33 +0100

    d

--- src/components/site/AdminShell.tsx
+++ src/components/site/AdminShell.tsx
@@ -44,6 +44,7 @@ const NAV_GROUPS: NavGroup[] = [
       { to: "/admin/shortlinks", label: "Liens courts", icon: Link2 },
       { to: "/admin/announces", label: "Annonces", icon: Flag },
       { to: "/admin/reviews/forms", label: "Formulaires d'avis", icon: Form, matchPrefixes: ["/admin/reviews"] },
+      { to: "/admin/jobs/offers", label: "Offres d'emploies", icon: Form, matchPrefixes: ["/admin/jobs"] },
     ],
   },
   {

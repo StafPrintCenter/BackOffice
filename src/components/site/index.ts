6baa84c796394b38f29ab1a1cc74fdb8159668f7
steveasterafovo <steveasterafovo@gmail.com>
2026-08-05 21:50:27 +0100

    x

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,5 +1,6 @@
 export * from "./AdminBits";
 export { AdminShell } from "./AdminShell";
+export { AuthShell } from "./AuthShell";
 export { DataTable } from "./DataTable";
 export { RichTextEditor } from "./RichTextEditor";
 export { FilePreviewModal } from "./FilePreviewModal";

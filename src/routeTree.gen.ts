57cd23caa09b263d16dba27f29e3254689048d0c
steveasterafovo <steveasterafovo@gmail.com>
2026-08-06 10:30:03 +0100

    s

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -455,7 +455,6 @@ export interface FileRoutesByTo {
   '/admin/messages': typeof AdminMessagesIndexRoute
   '/admin/projects': typeof AdminProjectsIndexRoute
   '/admin/reports': typeof AdminReportsIndexRoute
-  '/admin/services': typeof AdminServicesIndexRoute
   '/admin/shortlinks': typeof AdminShortlinksIndexRoute
   '/admin/stats': typeof AdminStatsIndexRoute
   '/admin/testimonials': typeof AdminTestimonialsIndexRoute

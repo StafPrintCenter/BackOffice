7fd2ba5878fdf66e34a1556d5bb5f0596c2129b4
steveasterafovo <steveasterafovo@gmail.com>
2026-08-06 10:31:13 +0100

    d

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -694,7 +694,6 @@ export interface FileRouteTypes {
     | '/admin/messages/'
     | '/admin/projects/'
     | '/admin/reports/'
-    | '/admin/services/'
     | '/admin/shortlinks/'
     | '/admin/stats/'
     | '/admin/testimonials/'

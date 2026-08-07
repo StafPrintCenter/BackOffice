9974db6933d2591ac9fb611581882e40db374191
steveasterafovo <steveasterafovo@gmail.com>
2026-08-07 22:00:49 +0100

    s

--- src/routes/_admin copy.tsx
+++ src/routes/_admin copy.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { Outlet, createFileRoute } from "@tanstack/react-router";
 import { AdminShell } from "@/components/site/AdminShell";
 
 export const Route = createFileRoute("/_admin copy")({

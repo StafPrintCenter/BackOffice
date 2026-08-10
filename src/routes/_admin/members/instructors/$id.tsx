33c4cc9e7db47ce11bdb3ff2097cc99393b951e1
steveasterafovo <steveasterafovo@gmail.com>
2026-08-10 10:15:45 +0100

    s

--- src/routes/_admin/members/instructors/$id.tsx
+++ src/routes/_admin/members/instructors/$id.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogDescription } from "@/components/ui/dialog";
 import { useAdminInstructorDetail, useApproveAdminInstructor, useResendAdminInstructorInvite, useRevokeAdminInstructorInvite, useAlertAdminInstructor, useBlockAdminInstructor, useReactivateAdminInstructor, } from "@/stores/useInstructorsStore";
 import { useRemoveAdminTrainingInstructorAssignment } from "@/stores/useTrainingInstructorsStore";

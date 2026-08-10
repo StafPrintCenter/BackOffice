13b33af7615346eee0459ad0ef1ccfdc214191a4
steveasterafovo <steveasterafovo@gmail.com>
2026-08-10 10:16:16 +0100

    s

--- src/routes/_admin/members/instructors/$id.tsx
+++ src/routes/_admin/members/instructors/$id.tsx
@@ -9,8 +9,14 @@ import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogDescription } from "@/components/ui/dialog";
-import { useAdminInstructorDetail, useApproveAdminInstructor, useResendAdminInstructorInvite, useRevokeAdminInstructorInvite, useAlertAdminInstructor, useBlockAdminInstructor, useReactivateAdminInstructor, } from "@/stores/useInstructorsStore";
-import { useRemoveAdminTrainingInstructorAssignment } from "@/stores/useTrainingInstructorsStore";
+import {
+  useAdminInstructorDetail, useApproveAdminInstructor, useResendAdminInstructorInvite,
+  useRevokeAdminInstructorInvite, useAlertAdminInstructor, useBlockAdminInstructor, useReactivateAdminInstructor,
+} from "@/stores/useInstructorsStore";
+import {
+  useAssignAdminTrainingInstructor, useRemoveAdminTrainingInstructorAssignment,
+} from "@/stores/useTrainingInstructorsStore";
+import { useAdminTrainingsList } from "@/stores/useTrainingsStore";
 import { TRAINING_INSTRUCTOR_ROLE_LABELS } from "@/data/trainingInstructors";
 import { SITE } from "@/data/site";
 

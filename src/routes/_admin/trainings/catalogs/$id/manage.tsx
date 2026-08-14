988ba89edfe7249ccc8415de3b936569e1167d4b
steveasterafovo <steveasterafovo@gmail.com>
2026-08-14 21:53:11 +0100

    d

--- src/routes/_admin/trainings/catalogs/$id/manage.tsx
+++ src/routes/_admin/trainings/catalogs/$id/manage.tsx
@@ -522,11 +522,11 @@ function TrainingManageDetail() {
     <>
       <div className="mb-6 flex flex-wrap items-center justify-between gap-3">
         <div className="flex flex-wrap gap-3">
-          <Button variant="ghost" size="sm" onClick={() => navigate({ to: "/trainings/catalogs/$id", params: { id: training.id } })}>
-            <ArrowLeft className="h-4 w-4 mr-1" /> Voir le contenu
-          </Button>
           <Button variant="outline" size="sm" onClick={() => navigate({ to: "/trainings/catalogs" })}>
             <ArrowLeft className="h-4 w-4 mr-1" /> Retour
+          </Button>
+          <Button variant="link" size="sm" onClick={() => navigate({ to: "/trainings/catalogs/$id", params: { id: training.id } })}>
+            Voir le contenu
           </Button>
         </div>
 

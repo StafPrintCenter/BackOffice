75eb9740f1dca1f103c7b7751931c0b673ef4b59
steveasterafovo <steveasterafovo@gmail.com>
2026-08-17 14:34:51 +0100

    s

---
+++ src/components/pages/admin/announces/detail/index.ts
@@ -0,0 +1,6 @@
+export * from "./DetailHeader";
+export * from "./ContentCard";
+export * from "./AnalyticsCard";
+export * from "./ConfigSidebarCard";
+export * from "./PublicationSidebarCard";
+export * from "./ActionSidebarCard";
